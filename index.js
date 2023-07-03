96142ee9563865c9a842fcae0ce26b4af3e8acc5
noizzer28 <colosova28@yandex.ru>
2023-07-03 15:46:17 +0400

    перенос верстки в javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+import { startMenu } from "./components/start-components.js"
+
+let gameLevel = [
+    EASY,
+    MEDIUM,
+    HARD
+]
+
+
+
+function startGame() {
+    startMenu()
+}
+
+
+startGame()
