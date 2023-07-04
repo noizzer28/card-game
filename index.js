7e0be1bebe190f297e575b34beff9c490b7375b1
noizzer28 <colosova28@yandex.ru>
2023-07-04 16:46:38 +0400

    верстка задней части колоды

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-import { renderGame } from "./components/startGame.js";
+// import { renderGame } from "./components/startGame.js";
 
 let gameLevel = null;
-const app = document.getElementById("app");
+const gamecontainer = document.querySelector(".game");
+const startContainer = document.getElementById("start");
 
+const cardsBack = [];
+const cardsFront = [];
 
 function renderStartPage() {
-  app.innerHTML = `<div class="start-box">
-    <h1 class="start-box-text">Выбери сложность</h1>
-    <form>
-        <div class="start-box-flex">
-            <label>
-                <input type="radio" class="start-box-radio" id="option1" name="option" value="1">
-                <div class="start-box-number">1</div>
-            </label>
-            <label>
-                <input type="radio" class="start-box-radio" id="option2" name="option" value="2">
-                <div class="start-box-number">2</div>
-            </label>
-            <label>
-                <input type="radio" class="start-box-radio" id="option3" name="option" value="3">
-                <div class="start-box-number">3</div>
-            </label>
-        </div>
-        <button class="start-box-button button">Старт</button>
-      </form>
-    </div>`;
-
+  gamecontainer.style.display = "none";
   const startButton = document.querySelector(".start-box-button");
   const menuOptions = document.querySelectorAll(".start-box-radio");
   for (const menuOption of menuOptions) {
     startButton.addEventListener("click", () => {
       if (menuOption.checked) {
         gameLevel = menuOption.value;
-        renderGame({
-            app,
-            gameLevel,
-        })
+        renderBackSide();
+        startContainer.style.display = "none";
+        gamecontainer.style.display = "block";
       }
     });
   }
+}
+
+function renderBackSide() {
+  const backSideElement = document.querySelector(".back-side");
+
+  for (let index = 0; index < 36; index++) {
+    const element = `<div class="card"></div>`;
+    cardsBack.push(element);
+  }
+  const cardsBackHTML = cardsBack.join("");
+  console.log(cardsBackHTML);
+  console.log(backSideElement);
+  backSideElement.innerHTML = cardsBackHTML;
 }
 
 renderStartPage();
