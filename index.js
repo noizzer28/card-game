98fbf00275bac47436989a4d0ce4dc5cc663205c
noizzer28 <colosova28@yandex.ru>
2023-07-06 19:41:30 +0400

    позиц-ние сторон, обработка клика по рубашке

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import Deck from "./components/deck.js";
 let gameLevel = null;
 const gamecontainer = document.querySelector(".game");
-const startContainer = document.getElementById("start");
+
 const deck = Object.values(new Deck()).flat();
 const cardsBack = [];
 
 function renderStartPage() {
-  gamecontainer.style.display = "none";
+  const startContainer = document.getElementById("start");
   const startButton = document.querySelector(".start-box-button");
   const menuOptions = document.querySelectorAll(".start-box-radio");
   for (const menuOption of menuOptions) {
-    startButton.addEventListener("click", () => {
+    startButton.addEventListener("click", (event) => {
+      event.preventDefault();
       if (menuOption.checked) {
         gameLevel = menuOption.value;
-        renderBackSide();
-        renderFrontSide();
+
         startContainer.style.display = "none";
         gamecontainer.style.display = "block";
+        renderBackSide();
+        renderFrontSide();
       }
     });
   }
 }
 
 function renderBackSide() {
+  console.log("here");
   const backSideElement = document.querySelector(".back-side");
 
   for (let index = 0; index < 36; index++) {
@@ -36,23 +39,38 @@ function renderBackSide() {
 }
 
 function renderFrontSide() {
+  console.log("here2");
   const frontSideElement = document.querySelector(".front-side");
   frontSideElement.innerHTML = deck
     .map((card) => {
       return ` 
-      <div class="card-front">
-    <div class="card-value-top">
-        <p>A</p>
-      ${card.suit.top}
-    </div>
-      ${card.suit.middle}
-    <div class="card-value-bottom">
-        <p>A</p>
-      ${card.suit.top}
-    </div>
-</div>`;
+      <div class="card-item-front">
+        <div class="card-front">
+            <div class="card-value-top">
+          <p>${card.value}</p>
+        ${card.suit.top}
+            </div>
+        ${card.suit.middle}
+            <div class="card-value-bottom">
+          <p>${card.value}</p>
+        ${card.suit.top}
+            </div>
+        </div>
+      </div>`;
     })
     .join("");
+
+  turnDeck();
 }
 
 renderStartPage();
+
+function turnDeck() {
+  const cardsFront = document.querySelectorAll(".card-item-front");
+  console.log(cardsFront);
+  for (const cardFront of cardsFront) {
+    cardFront.addEventListener("click", () => {
+      cardFront.style.zIndex = "1";
+    });
+  }
+}
